1e48731139c4671b28c87621ade192e4ef7891ac
NathanPeereboom <47399969+NathanPeereboom@users.noreply.github.com>
2019-05-10 14:42:19 -0400

    Add files via upload

---
+++ Coin.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing;
+
+
+namespace rocketRiotv2
+{
+    public class Coin
+    {
+        int quadrant;
+        Random random;
+        Canvas canvas = new Canvas();
+        public Rectangle coin = new Rectangle();
+        BitmapImage bitmapImage;
+        ImageBrush coinFill;
+        
+        
+        public Coin(int q, Canvas c, Random r)
+        {
+            quadrant = q;
+            canvas = c;
+            random = r;
+            //quadrant is not yet used
+        }
+        public void generate(int setX, int setY)
+        {
+            //Image
+            bitmapImage = new BitmapImage(new Uri("coin.png", UriKind.Relative));
+            coinFill = new ImageBrush(bitmapImage);
+            coin.Fill = coinFill;
+
+            //Coin size
+            coin.Height = 20;
+            coin.Width = 20;
+
+            //Set Position
+            
+            Canvas.SetLeft(coin, setX + quadrant * 200);
+            Canvas.SetTop(coin, setY);
+
+            //Add to canvas
+            canvas.Children.Add(coin);
+        }
+
+        public void collected()
+        {
+
+        }
+
+        public void remove()
+        {
+            canvas.Children.Remove(coin);
+        }
+
+        public Rectangle specs()
+        {
+            return coin;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace rocketRiotv2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random random = new Random();
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<Coin> Coins = new List<Coin>();
+            for (int i = 0; i < 4; i++)
+            {
+                Coins.Add(new Coin(i, canvas, random));
+            }
+
+            foreach (Coin coin in Coins)
+            {
+                coin.generate(random.Next(40 , 140), random.Next(100 , 500));
+            }
+
+            
+        }
+    }
+}
